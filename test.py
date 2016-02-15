9ec5c33aacfed3b9f1dfb55e989d782201e49ee6
Jack <jack.grahl@yahoo.co.uk>
2016-02-15 22:51:56 +0000

    Compare return codes.
    Do not worry about error messages yet.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import unittest
 class CompareErrorMessages(unittest.TestCase):
     def test_missing_file(self):
         args = ['./lss.sh', 'foo']
-        msg = subprocess.check_output(args, stderr=subprocess.STDOUT)
+        ret = subprocess.call(args)
         args2 = ['ls', 'foo']
-        msg2 = subprocess.check_output(args2, stderr=subprocess.STDOUT)
-        self.assertEqual(msg, msg2)
+        ret2 = subprocess.call(args2)
+        self.assertEqual(ret, ret2)
