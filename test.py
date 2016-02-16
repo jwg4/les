d05a33851ec59b11f0799cb6d1b45a55e733b6c5
Jack <jack.grahl@yahoo.co.uk>
2016-02-16 19:32:54 +0000

    Add a new test to compare outputs.

--- test.py
+++ test.py
@@ -2,9 +2,22 @@ import subprocess
 import unittest
 
 class CompareErrorMessages(unittest.TestCase):
-    def test_missing_file(self):
+    def test_missing_file_return_code_the_same_as_ls(self):
         args = ['./lss.sh', 'foo']
         ret = subprocess.call(args)
         args2 = ['ls', 'foo']
         ret2 = subprocess.call(args2)
         self.assertEqual(ret == 0, ret2 == 0)
+
+    def test_missing_file_message_code_the_same_as_ls(self):
+        args = ['./lss.sh', 'foo']
+        try:
+            subprocess.check_output(args, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            msg = e.output
+        args2 = ['ls', 'foo']
+        try:
+            subprocess.check_output(args2, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            msg2 = e.output
+        self.assertEqual(msg, msg2)
